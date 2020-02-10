e0742a0ed984eb8d62a2f1f573d6468c72fc01b5
Taras Kustov <tkusto@gmail.com>
2020-02-10 17:25:17 +0200

    feat: add built assets

---
+++ build/precache-manifest.cf5316661bb458a1984f1ef13ebc5dc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "941a96d6c2e45501c5aec72517c9c7ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "260292fdd54fe48760b4",
+    "url": "/static/css/main.5ab99c0a.chunk.css"
+  },
+  {
+    "revision": "ce2fff702a88fadd06c6",
+    "url": "/static/js/2.1032ae58.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.1032ae58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "260292fdd54fe48760b4",
+    "url": "/static/js/main.d507b406.chunk.js"
+  },
+  {
+    "revision": "ed0be8f1ff84079cb5c8",
+    "url": "/static/js/runtime-main.61c45327.js"
+  }
+]);
